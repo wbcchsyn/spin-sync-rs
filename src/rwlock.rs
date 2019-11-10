6f285ea66b9bd32baa2ee2d571239cd22928e18d
Yoshida Shin <wbcchsyn@gmail.com>
2019-11-11 01:07:25 +0900

    Implement Display for RwLockReadGuard

--- src/rwlock.rs
+++ src/rwlock.rs
@@ -466,6 +466,12 @@ impl<T: ?Sized> Drop for RwLockReadGuard<'_, T> {
     }
 }
 
+impl<T: ?Sized + fmt::Display> fmt::Display for RwLockReadGuard<'_, T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        (**self).fmt(f)
+    }
+}
+
 /// RAII structure used to release the exclusive write access of a lock when
 /// dropped.
 ///
