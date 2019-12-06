01d946e1f06fb383aeb86bacc9f92533a415399f
Yoshida Shin <wbcchsyn@gmail.com>
2019-12-06 16:04:24 +0900

    Update version in README.md and crate document

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! 1. Add the following line in dependencies section in your Cargo.toml.
 //!
 //!    ```Cargo.toml
-//!    spin-sync = "0.1.0"
+//!    spin-sync = "0.1.1"
 //!    ```
 //!
 //! 1. Build, test and run your project.
