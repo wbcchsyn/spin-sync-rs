17d52208374139b2644417f5299be9fcf68e988b
Yoshida Shin <wbcchsyn@gmail.com>
2020-07-25 00:37:17 +0900

    Create aliases to some PhantomData

--- src/misc.rs
+++ src/misc.rs
@@ -1,5 +1,5 @@
 use std::marker::PhantomData;
-use std::sync::MutexGuard;
+use std::sync::{Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 type NotSend<'a, T> = MutexGuard<'a, T>;
 
@@ -8,3 +8,10 @@ type NotSend<'a, T> = MutexGuard<'a, T>;
 /// Structs will implement !Send automatically to own property of this type
 /// without consuming any memory.
 pub type PhantomNotSend<'a, T> = PhantomData<NotSend<'a, T>>;
+
+/// PhantomData to implement !Send and so on.
+pub type PhantomMutex<T> = PhantomData<Mutex<T>>;
+pub type PhantomMutexGuard<'a, T> = PhantomData<MutexGuard<'a, T>>;
+pub type PhantomRwLock<T> = PhantomData<RwLock<T>>;
+pub type PhantomRwLockReadGuard<'a, T> = PhantomData<RwLockReadGuard<'a, T>>;
+pub type PhantomRwLockWriteGuard<'a, T> = PhantomData<RwLockWriteGuard<'a, T>>;
