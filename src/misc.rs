715e068b0bbbe35ff040bd262ab3589e54edc6b4
Yoshida Shin <wbcchsyn@gmail.com>
2020-07-25 00:54:30 +0900

    Delete unused type PhantomNotSend

--- src/misc.rs
+++ src/misc.rs
@@ -1,15 +1,10 @@
 use std::marker::PhantomData;
 use std::sync::{Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
-type NotSend<'a, T> = MutexGuard<'a, T>;
-
 /// PhantomData implementing !Send
 ///
 /// Structs will implement !Send automatically to own property of this type
 /// without consuming any memory.
-pub type PhantomNotSend<'a, T> = PhantomData<NotSend<'a, T>>;
-
-/// PhantomData to implement !Send and so on.
 pub type PhantomMutex<T> = PhantomData<Mutex<T>>;
 pub type PhantomMutexGuard<'a, T> = PhantomData<MutexGuard<'a, T>>;
 pub type PhantomRwLock<T> = PhantomData<RwLock<T>>;
