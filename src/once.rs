412d5fba9cd46943ecf4725fbcf532677a510fd4
Yoshida Shin <wbcchsyn@gmail.com>
2020-07-25 17:04:51 +0900

    Create methods for OnceState

--- src/once.rs
+++ src/once.rs
@@ -15,3 +15,63 @@ pub struct Once {
 pub struct OnceState {
     state: u8,
 }
+
+impl OnceState {
+    const INIT: u8 = 0;
+    const LOCK: u8 = 1;
+    const FINISHED: u8 = 2;
+    const POISONED: u8 = 4;
+
+    #[must_use]
+    const fn default() -> Self {
+        Self { state: Self::INIT }
+    }
+
+    #[must_use]
+    const fn new(state: u8) -> Self {
+        Self { state }
+    }
+
+    #[must_use]
+    const fn locked(&self) -> bool {
+        (self.state & Self::LOCK) != 0
+    }
+
+    #[must_use]
+    const fn finished(&self) -> bool {
+        (self.state & Self::FINISHED) != 0
+    }
+
+    #[must_use]
+    const fn poisoned(&self) -> bool {
+        (self.state & Self::POISONED) != 0
+    }
+
+    #[must_use]
+    fn acquire_lock(&self) -> Self {
+        debug_assert!(!self.locked());
+        Self::new(self.state | Self::LOCK)
+    }
+
+    #[must_use]
+    fn release_lock(&self) -> Self {
+        debug_assert!(self.locked());
+        Self::new(self.state ^ Self::LOCK)
+    }
+
+    #[must_use]
+    fn finish(&self) -> Self {
+        debug_assert!(!self.finished());
+        Self::new(self.state | Self::FINISHED)
+    }
+
+    #[must_use]
+    fn poison(&self) -> Self {
+        Self::new(self.state | Self::POISONED)
+    }
+
+    #[must_use]
+    fn unpoison(&self) -> Self {
+        Self::new(self.state ^ Self::POISONED)
+    }
+}
