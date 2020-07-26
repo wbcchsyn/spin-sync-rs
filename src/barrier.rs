99835ece83dc971e65322f836060ec323135ac49
Yoshida Shin <wbcchsyn@gmail.com>
2020-07-27 02:05:40 +0900

    Create struct Barrier

--- src/barrier.rs
+++ src/barrier.rs
@@ -1,1 +1,7 @@
+use std::sync::atomic::AtomicUsize;
 
+pub struct Barrier {
+    num_threads: usize, // immutable
+    count: AtomicUsize,
+    generation_id: AtomicUsize, // MSB plays lock flag role.
+}
