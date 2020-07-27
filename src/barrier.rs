7ed244e6c0d3abc5bdc37f54f79ee95e5ed5bd38
Yoshida Shin <wbcchsyn@gmail.com>
2020-07-27 11:56:39 +0900

    Implement Drop for BarrierLockGuard

--- src/barrier.rs
+++ src/barrier.rs
@@ -1,4 +1,4 @@
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct Barrier {
     num_threads: usize, // immutable
@@ -20,4 +20,19 @@ pub struct BarrierWaitResult(bool);
 
 struct BarrierLockGuard<'a> {
     generation_id: &'a AtomicUsize,
+}
+
+impl BarrierLockGuard<'_> {
+    const MSB: usize = usize::MAX / 2 + 1;
+}
+
+impl Drop for BarrierLockGuard<'_> {
+    // Make sure to unlock
+    fn drop(&mut self) {
+        let current = self.generation_id.load(Ordering::Relaxed);
+        debug_assert_eq!(Self::MSB, current & Self::MSB);
+
+        let desired = current - Self::MSB;
+        self.generation_id.store(desired, Ordering::Release);
+    }
 }
